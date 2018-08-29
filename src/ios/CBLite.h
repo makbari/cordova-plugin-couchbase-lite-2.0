4d883011fadb5bbdf5bc02a5653b63a98f2d3620
rickymediaengine <riccardo.barbi@mediaengine.it>
2018-08-29 17:18:58 +0200

    Remove unused

--- src/ios/CBLite.h
+++ src/ios/CBLite.h
@@ -11,8 +11,6 @@
 - (void)info:(CDVInvokedUrlCommand*)urlCommand;
 - (void)initDb:(CDVInvokedUrlCommand*)urlCommand;
 - (void)lastSequence:(CDVInvokedUrlCommand*)urlCommand;
-- (void)replicateFrom:(CDVInvokedUrlCommand*)urlCommand;
-- (void)replicateTo:(CDVInvokedUrlCommand*)urlCommand;
 - (void)reset:(CDVInvokedUrlCommand*)urlCommand;
 - (void)stopReplication:(CDVInvokedUrlCommand*)urlCommand;
 - (void)sync:(CDVInvokedUrlCommand*)urlCommand;
